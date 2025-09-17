e7bf8407593267022198a2f465e67b4345ef42ed
Gabriel Dantas <gdantaslopes62@gmail.com>
2025-09-17 16:44:38 -0300

    fix: up question file

--- topinterview150/a.cpp
+++ topinterview150/a.cpp
@@ -1,7 +1,11 @@
-#include <bits/stdc++.h>
-using namespace std;
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        for(int i = 0; i < m; i++) 
+            if(nums1[i] == 0) nums1[i] = nums2[i-n];
 
-int main(){
-    vector<int> nums1, nums2; cin >> n; 
-    return 0;
-}
+        sort(nums1.begin(), nums1.end());
+
+        for(auto& a : nums1) cout << a << " ";    
+    }
+};
